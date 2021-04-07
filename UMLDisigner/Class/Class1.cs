7cc41f9d2426d5a391a73d7011c8361e8fcfcdab
Sergey <tootoo97@gmail.com>
2021-04-07 12:49:25 +0300

    Added4ClassTables

--- UMLDisigner/Class/Class1.cs
+++ UMLDisigner/Class/Class1.cs
@@ -5,12 +5,15 @@ using System.Drawing;
 
 namespace UMLDisigner
 {
-    class Rectangle : IFigure
+    class Class1 : IFigure
     {
         public void Draw(Graphics graphics, Pen pen, Points p)
         {
+          //  Font drawFont = new Font("Arial", 16);
+         //   SolidBrush drawBrush = new SolidBrush(Color.Black);
             graphics.DrawPolygon(pen, new Point[] { new Point(p.Positions[0].X, p.Positions[0].Y),new Point(p.Positions[0].X, p.Positions[1].Y),
         new Point(p.Positions[1].X, p.Positions[1].Y),new Point(p.Positions[1].X, p.Positions[0].Y)});
+          //  graphics.DrawString("Text", drawFont, drawBrush, new Point(p.Positions[0].X+10, p.Positions[0].Y+10));
         }
     }
 }
---
+++ UMLDisigner/Class/Class2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace UMLDisigner
+    {
+        class Class2 : IFigure
+        {
+            public void Draw(Graphics graphics, Pen pen, Points p)
+            {
+            int topLineHeight = 30;
+                graphics.DrawPolygon(pen, new Point[] { new Point(p.Positions[0].X, p.Positions[0].Y),new Point(p.Positions[0].X, p.Positions[1].Y),
+        new Point(p.Positions[1].X, p.Positions[1].Y),new Point(p.Positions[1].X, p.Positions[0].Y)});
+               if((p.Positions[0].Y- p.Positions[1].Y) > topLineHeight)
+                {
+                graphics.DrawLine(pen, new Point(p.Positions[0].X, p.Positions[1].Y + topLineHeight), 
+                    new Point(p.Positions[1].X, p.Positions[1].Y+ topLineHeight));
+                 }
+               else if ((p.Positions[1].Y - p.Positions[0].Y) > topLineHeight)
+            {
+                graphics.DrawLine(pen, new Point(p.Positions[0].X, p.Positions[0].Y + topLineHeight),
+                    new Point(p.Positions[1].X, p.Positions[0].Y + topLineHeight));
+            }
+            }
+        }
+    }
+
+
